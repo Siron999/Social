198007e89aefd1a0a50286bb82f8e6bd4c148d75
Siron <sironshakya10@gmail.com>
2021-08-01 00:23:55 +0545

    test

--- config/db.js
+++ config/db.js
@@ -2,7 +2,11 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize('db1d9ok335qra7', 'ofpjouoztujugu', '21096de98a18c37dc1bfa3d1b6c90a624cb92864e6337dc606c7abc7763b0d76', {
     host: 'ec2-54-195-76-73.eu-west-1.compute.amazonaws.com',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
 });
 
 module.exports = sequelize;
