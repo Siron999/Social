41846565c4d69e8641e10b207cad89c1bf6adc01
Siron <sironshakya10@gmail.com>
2021-09-08 18:05:04 +0545

    test

--- utility/logger.js
+++ utility/logger.js
@@ -2,13 +2,13 @@ const winston = require('winston');
 require('winston-mongodb');
 
 const logger = winston.createLogger({
-    transports: [
-        new winston.transports.MongoDB({
-            db: 'mongodb+srv://siron:abcde12345@cluster0.dw0ms.mongodb.net/<dbname>?retryWrites=true&w=majority',
-            level: 'info',
-            collection:'logs'
-        }),
-    ],
+    // transports: [
+    //     new winston.transports.MongoDB({
+    //         db: 'mongodb+srv://siron:abcde12345@cluster0.dw0ms.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    //         level: 'info',
+    //         collection:'logs'
+    //     }),
+    // ],
 });
 
 module.exports = logger;
